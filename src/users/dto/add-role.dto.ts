99f2c17d093dbf93e9d165ebfbc973a807090657
unknown <R.1on@yandex.ru>
2021-11-19 00:23:49 +0300

    first commit

---
+++ src/users/dto/add-role.dto.ts
@@ -0,0 +1,8 @@
+import {IsNumber, IsString} from 'class-validator';
+
+export class AddRoleDto {
+    @IsNumber({}, { message: 'Должно быть числом' })
+    readonly userId: number;
+    @IsString({ message: 'Должно быть строкой' })
+    readonly value: string;
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,51 @@
+import {Body, Controller, Get, Param, Post, UseGuards} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './users.model';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import {Roles} from '../auth/roles-auth.decorator';
+import {AddRoleDto} from './dto/add-role.dto';
+import {BanUserDto} from './dto/ban-user.dto';
+import {RolesGuard} from '../auth/roles.guard';
+
+@Controller('users')
+export class UsersController {
+
+    constructor(private userService: UsersService) {}
+
+    @ApiOperation({ summary: 'Создание пользователя' })
+    @ApiResponse({ status: 200, type: User })
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+        return this.userService.createUser(userDto);
+    }
+
+    @ApiOperation({ summary: 'Получение всех пользователей' })
+    @ApiResponse({ status: 200, type: [User] })
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard)
+    @Get()
+    getAll() {
+        return this.userService.getAllUsers();
+    }
+
+    @ApiOperation({ summary: 'Выдать роль' })
+    @ApiResponse({ status: 200 })
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard)
+    @Post('/role')
+    addRole(@Body() dto: AddRoleDto) {
+        return this.userService.addRole(dto);
+    }
+
+    @ApiOperation({ summary: 'Забанить пользователя' })
+    @ApiResponse({ status: 200 })
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard)
+    @Post('/ban')
+    ban(@Body() dto: BanUserDto) {
+        console.log(dto)
+        return this.userService.ban(dto);
+    }
+}
